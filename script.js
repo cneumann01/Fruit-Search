4bb5a6ffc4dfce2a2b692e339da43c8d50ba0290
cneumann01 <carterneumann@protonmail.com>
2023-03-08 16:51:31 -0500

    fixed bug where empty search returned entire array

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const suggestions = document.querySelector('.suggestions ul');
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
 function search(str) {
-	// Compares lower-cased version of fruit item to lower-cased version of search string
-	let results = fruit.filter(item => item.toLowerCase().includes(str.toLowerCase()))
-	return results
+	// Checks if input string is empty.
+	if (input.value !== '') {
+		// Compares lower-cased version of fruit item to lower-cased version of search string
+		let results = fruit.filter(item => item.toLowerCase().includes(str.toLowerCase()))
+		return results
+	} else {
+		return []
+	}
+
 }
 
 function searchHandler(e) {
-	if (input.value !== '') {
-		showSuggestions(search(input.value), input.value)	
-	}
+	showSuggestions(search(input.value), input.value)
 }
 
 function showSuggestions(results, inputVal) {
